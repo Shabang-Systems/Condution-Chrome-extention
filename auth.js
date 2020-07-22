299df295a059f6ce45881e42ca5b1a0f30f1c579
Micah Robinson <micahrobinson93@gmail.com>
2020-07-21 17:49:16 -0700

    starting sign in

---
+++ auth.js
@@ -0,0 +1,8 @@
+//require firebase code
+
+firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // ...
+});
